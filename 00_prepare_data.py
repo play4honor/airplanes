47f7c3225e492ad00b19d876d061dbd4af41b66f
DoubleSec <awzack@gmail.com>
2026-01-30 00:41:33 -0600

    initial commit

---
+++ 00_prepare_data.py
@@ -0,0 +1,96 @@
+import polars as pl
+
+
+def prepare_flight_chain_data(
+    flight_data: list[str],
+    supplemental_data: str,
+    output_path: str,
+):
+    # Read data and correct dates.
+    initial_data = pl.concat(
+        [pl.read_csv(fd_path) for fd_path in flight_data]
+    ).with_columns(
+        pl.col("FL_DATE")
+        .str.strptime(pl.Date, "%Y-%m-%d %H:%M:%S")
+        .dt.strftime("%Y%m%d")
+        .alias("gross_flight_date"),
+        pl.col("CRS_DEP_TIME")
+        .str.strptime(pl.Time, "%Y-%m-%d %H:%M:%S")
+        .dt.strftime("%-H%M"),
+        pl.col("DEP_TIME").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"),
+        pl.col("FL_DATE").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"),
+        pl.col("ARR_TIME").str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S"),
+    )
+
+    supplemental_data = (
+        pl.read_parquet(supplemental_data)
+        .filter(pl.col("flight_number") != "")
+        .with_columns(pl.col("flight_number").cast(pl.Float64))
+    )
+
+    flight_data = (
+        initial_data.join(
+            supplemental_data,
+            left_on=[
+                "gross_flight_date",
+                "OP_CARRIER",
+                "OP_CARRIER_FL_NUM",
+                "CRS_DEP_TIME",
+            ],
+            right_on=["date", "carrier", "flight_number", "departure_time"],
+        )
+        .with_columns(
+            pl.concat_list(
+                pl.concat_str(pl.lit("ORIGIN="), pl.col("ORIGIN")),
+                pl.concat_str(pl.lit("DESTINATION="), pl.col("DEST")),
+                pl.concat_str(
+                    pl.lit("DEP_TIME="),
+                    ((pl.col("DEP_TIME") - pl.col("FL_DATE")).dt.total_minutes() / 10)
+                    .cast(pl.Int32)
+                    .alias("departure_time"),
+                ),
+                pl.concat_str(
+                    pl.lit("DEP_DELAY="),
+                    pl.col("DEP_DELAY").sign()
+                    * pl.col("DEP_DELAY").abs().sqrt().ceil(),
+                ),
+                pl.concat_str(
+                    pl.lit("ARR_TIME="),
+                    ((pl.col("ARR_TIME") - pl.col("FL_DATE")).dt.total_minutes() / 10)
+                    .cast(pl.Int32)
+                    .alias("arrival_time"),
+                ),
+                pl.concat_str(
+                    pl.lit("ARR_DELAY="),
+                    pl.col("ARR_DELAY").sign()
+                    * pl.col("ARR_DELAY").abs().sqrt().ceil(),
+                ),
+            ).alias("flight_info")
+        )
+        .sort(["FL_DATE", "DEP_TIME"], descending=False)
+        .group_by("FL_DATE", "tail_number")
+        .agg(pl.col("flight_info").flatten().alias("flight_info"))
+        .with_columns(
+            pl.concat_list(
+                pl.lit("<SOS>"), pl.col("flight_info"), pl.lit("<EOS>")
+            ).alias("flight_info")
+        )
+    )
+
+    print(f"Number of flight chains: {flight_data.height}")
+    print(flight_data.row(0, named=True))
+    flight_data.write_parquet(output_path)
+
+
+if __name__ == "__main__":
+
+    prepare_flight_chain_data(
+        flight_data=[
+            "./data/Flight_Tab/flight_with_weather_2021.csv",
+            "./data/Flight_Tab/flight_with_weather_2022.csv",
+            "./data/Flight_Tab/flight_with_weather_2023.csv",
+            "./data/Flight_Tab/flight_with_weather_2024.csv",
+        ],
+        supplemental_data="./data/supplemental.parquet",
+        output_path="./data/prepared_data.parquet",
+    )
---
+++ src/data.py
@@ -0,0 +1,74 @@
+import torch
+from torch.utils.data import Dataset
+import polars as pl
+
+
+class Tokenizer:
+
+    def __init__(self, tokens: list[str]):
+
+        self.mapping = {token: i for i, token in enumerate(sorted(set(tokens)))}
+        features = set(token.split("=")[0] for token in self.mapping.keys())
+        for feature in features:
+            self.mapping[f"{feature}=UNK"] = len(self.mapping)
+        self.mapping["<PAD>"] = len(self.mapping)
+
+    def to_index(self, token_sequence: list[str]):
+
+        return torch.tensor(
+            [
+                self.mapping.get(token, self.mapping[f"{token.split('=')[0]}=UNK"])
+                for token in token_sequence
+            ],
+            dtype=torch.long,
+        )
+
+
+class FlightsDataset(Dataset):
+
+    def __init__(self, data: pl.DataFrame, max_length: int, tokenizer: Tokenizer):
+        super().__init__()
+
+        self.data = data
+        self.max_length = max_length
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return self.data.height
+
+    def __getitem__(self, idx):
+        row = self.data.row(idx, named=True)
+        indices = self.tokenizer.to_index(row["flight_info"])[: self.max_length]
+        indices = torch.nn.functional.pad(
+            indices,
+            [0, self.max_length - indices.shape[0]],
+            value=self.tokenizer.mapping["<PAD>"],
+        )
+        return {
+            "flight_date": row["FL_DATE"],
+            "tail_number": row["tail_number"],
+            "event_sequence": indices,
+        }
+
+
+if __name__ == "__main__":
+
+    import polars as pl
+
+    data = pl.read_parquet("./data/prepared_data.parquet")
+    print(data.with_columns(_len=pl.col("flight_info").list.len()).max()["_len"])
+
+    vocabulary = (
+        data.select(pl.col("flight_info").explode().alias("tokens"))
+        .unique()["tokens"]
+        .to_list()
+    )
+
+    tokenizer = Tokenizer(vocabulary)
+    ds = FlightsDataset(
+        data,
+        max_length=86,
+        tokenizer=tokenizer,
+    )
+
+    print(ds[500])
